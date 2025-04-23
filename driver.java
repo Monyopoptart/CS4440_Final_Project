c378a59d0e827b2b98a270ee8a6aa8b999c12148
Harshaun <harshaunkhehra@yahoo.com>
2025-04-23 12:07:27 -0700

    picking word in thread

--- driver.java
+++ driver.java
@@ -1,8 +1,14 @@
+
 public class driver {
   public static void main(String[] args) {
     ReadWord reader = new ReadWord("words.txt");
 
     String word = reader.getRandomWord();
-    System.out.println("Picked word: " + word);
+    
+    Thread thread = new Thread(() -> {
+      System.out.println("Picked word: " + word);
+    });
+
+    thread.start();
   }
 }
